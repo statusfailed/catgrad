e3d784ab68ca7706e2cd3155ad48be3108e728aa
statusfailed <paul@statusfailed.com>
2023-11-27 20:08:17 +0000

    hypothesis: print reproducing example by default

--- conftest.py
+++ conftest.py
@@ -1,2 +1,8 @@
 from hypothesis import settings, Phase
-settings.register_profile("failfast", phases=[Phase.generate])
+values = settings.get_profile("default").__dict__ | {"print_blob": True}
+settings.register_profile("dev", settings(**values))
+settings.register_profile("failfast", phases=[Phase.generate], print_blob=True)
+
+# TODO: switch to something like this:
+# settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "default"))
+settings.load_profile("dev")
