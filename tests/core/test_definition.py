b7d38adb3b36ccaca198b9c856bc5aa8b84779de
statusfailed <paul@statusfailed.com>
2024-03-07 13:06:46 +0000

    another test for core definitions

--- tests/core/test_definition.py
+++ tests/core/test_definition.py
@@ -1,5 +1,7 @@
 from catgrad.signature import Dtype, op
 from catgrad.core.definition import Sigmoid
+from catgrad.core.operation import Add, Negate
+
 from catgrad.special.definition import inline
 from catgrad.target.python import to_python_function
 
@@ -40,4 +42,18 @@ def test_definition_sigmoid(Tx):
 
     actual = f(x)
     expected = [expit(x)]
+    assert_equal(actual, expected, exact=False)
+
+@pytest.mark.filterwarnings("ignore:overflow")
+@pytest.mark.filterwarnings("ignore:invalid value")
+@given(ndarrays(n=2, array_type=ndarraytypes(dtype=st.just(Dtype.float32))))
+def test_definition_sigmoid_complex(Tx):
+    """ a more complex example involving ``catgrad.core.definition.Sigmoid`` """
+    T, [x, y] = Tx
+    c = op(Add(T)) >> op(Sigmoid(T)) >> op(Negate(T))
+
+    f = to_python_function(c)
+
+    actual = f(x, y)
+    expected = [-expit(x+y)]
     assert_equal(actual, expected, exact=False)
